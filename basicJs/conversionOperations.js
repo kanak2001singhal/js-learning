8fa0c6d14100dcda4d96e778a07446a424e66717
kanak2001singhal <kanak2001singhal@gmail.com>
2026-03-30 15:28:53 +0000

    conversion operation in js

---
+++ basicJs/conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "kank"//13
+//console.log( typeof score);
+ let valueInNumber = Number(score)
+//console.log(valueInNumber);
+ 
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+let isLoggedIn = "kank"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "kanak" => true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
