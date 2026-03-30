caf95731148bfac06302fc2d0f4e6e4d6b839e85
kanak2001singhal <kanak2001singhal@gmail.com>
2026-03-30 12:00:08 +0000

    datatype in js

---
+++ basicJs/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+
+//alert(3 + 3) // it will not work as we are using not js not browser
+
+let name = "Kanak"
+let age = 24
+let isLoggedIn = true
+let state; //undefined
+
+
+//number => 2 to power 52 
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+//object
+console.log(typeof age);
